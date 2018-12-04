37fc47cf92301f774430f7c769de2e515845aaf5
“zhoufangbing” <1978467950@qq.com>
2018-12-04 12:57:05 +0800

    项目配置

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Button>1</Button>
       </div>
     );
   }

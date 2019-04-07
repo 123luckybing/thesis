de46051d3303fdae355d6b6a204ea64db2de0801
周芳冰 <zhoufangbing@daojia-inc.com>
2019-04-07 21:02:48 +0800

    test

---
+++ src/Recommend/MajorRecommend/data.js
@@ -0,0 +1,170 @@
+const data = [{
+  title: '你倾向从何处得到力量：',
+  valueFirst: 'E',
+  valueSecond: 'I',
+  contentFirst: '别人',
+  contentSecond: '自己的想法'
+}, {
+  title: '当你参加一个社交聚会时，你会：',
+  valueFirst: 'E',
+  valueSecond: 'I',
+  contentFirst: '在夜色很深时，一旦你开始投入，也许就是最晚离开的那一个。',
+  contentSecond: '在夜晚刚开始的时候，我就疲倦了并且想回家。'
+},{
+  title: '下列哪一件事听起来比较吸引你？',
+  valueFirst: 'E',
+  valueSecond: 'I',
+  contentFirst: '与另一半到有很多人且社交活动频繁的地方。',
+  contentSecond: '待在家中与恋人做一些特别的事情，例如说观赏一部有趣的录影带并享用你最喜欢的外卖食物。'
+},{
+  title: '在约会中，你通常：',
+  valueFirst: 'E',
+  valueSecond: 'I',
+  contentFirst: '整体来说很健谈。',
+  contentSecond: '较安静并保留，直到你觉得舒服。'
+},{
+  title: '过去，你遇见你大部分的恋人是：',
+  valueFirst: 'E',
+  valueSecond: 'I',
+  contentFirst: '在宴会中、夜总会、工作上、休闲活动中、会议上或当朋友介绍我给他们的朋友时。',
+  contentSecond: '通过私人的方式，例如个人广告、录影约会，或是由亲密的朋友和家人介绍。'
+},{
+  title: '你倾向拥有：',
+  valueFirst: 'E',
+  valueSecond: 'I',
+  contentFirst: '很多认识的人和很亲密的朋友。',
+  contentSecond: '一些很亲密的朋友和一些认识的人'
+},{
+  title: '过去，你的爱人或恋人倾向对你说：',
+  valueFirst: 'E',
+  valueSecond: 'I',
+  contentFirst: '你难道不可以安静一会儿吗？',
+  contentSecond: '可以请你从你的世界中出来一下吗？'
+},{
+  title: '你倾向通过以下哪种方式收集信息：',
+  valueFirst: 'N',
+  valueSecond: 'S',
+  contentFirst: '你对有可能发生之事的想像和期望。',
+  contentSecond: '你对目前状况的实际认知。'
+},{
+  title: '你倾向相信：',
+  valueFirst: 'N',
+  valueSecond: 'S',
+  contentFirst: '你的直觉。',
+  contentSecond: '你直接的观察和现成的经验。'
+},{
+  title: '当你置身于一段关系中时，你倾向相信：',
+  valueFirst: 'N',
+  valueSecond: 'S',
+  contentFirst: '永远有进步的空间。',
+  contentSecond: '若它没有被破坏，不予修补。'
+},{
+  title: '当与一个人交往时，你倾向于看重：',
+  valueFirst: 'F',
+  valueSecond: 'T',
+  contentFirst: '情感上的相容性：表达爱意和对另一半的需求很敏感。',
+  contentSecond: '智慧上的相容性：沟通重要的想法；客观地讨论和辩论事情。'
+},{
+  title: '当你对一个约会觉得放心时，你偏向谈论：',
+  valueFirst: 'N',
+  valueSecond: 'S',
+  contentFirst: '未来，关于改进或发明事物和生活的种种可能性。例如，你也许会谈论一个新的科学发明，或一个更好的方法来表达你的感受。',
+  contentSecond: '实际的、具体的、关于“此时此地”的事物。例如，你也许会谈论品酒的好方法，或你即将要参加的新奇旅程。'
+},{
+  title: '你是这种人: ',
+  valueFirst: 'N',
+  valueSecond: 'S',
+  contentFirst: '喜欢先纵观全局。',
+  contentSecond: '喜欢先掌握细节。'
+},{
+  title: '你是这类型的人：',
+  valueFirst: 'N',
+  valueSecond: 'S',
+  contentFirst: '与其活在现实中，不如活在想像里。',
+  contentSecond: '与其活在想像里，不如活在现实中。'
+},{
+  title: '你通常：',
+  valueFirst: 'N',
+  valueSecond: 'S',
+  contentFirst: '偏向于去想像一大堆关于即将来临的约会的事情。',
+  contentSecond: '偏向于拘谨地想像即将来临的约会，只期待让它自然地发生。 '
+},{
+  title: '你倾向如此做决定：',
+  valueFirst: 'F',
+  valueSecond: 'T',
+  contentFirst: '首先依你的心意，然后依你的逻辑。',
+  contentSecond: '首先依你的逻辑，然后依你的心意。'
+},{
+  title: '你倾向比较能够察觉到：',
+  valueFirst: 'F',
+  valueSecond: 'T',
+  contentFirst: '当人们需要情感上的支持时。',
+  contentSecond: '当人们不合逻辑时。'
+},{
+  title: '当和某人分手时：',
+  valueFirst: 'F',
+  valueSecond: 'T',
+  contentFirst: '你通常让自己的情绪深陷其中，很难抽身出来。',
+  contentSecond: '虽然你觉得受伤，但一旦下定决心，你会直截了当地将过去恋人的影子甩开。'
+},{
+  title: '当你不同意恋人人的想法时：',
+  valueFirst: 'F',
+  valueSecond: 'T',
+  contentFirst: '你尽可能地避免伤害对方的感情；若是会对对方造成伤害的话，你就不会说。',
+  contentSecond: '你通常毫无保留地说话，并且对恋人直言不讳，因为对的就是对的。'
+},{
+  title: '认识你的人倾向形容你为：',
+  valueFirst: 'F',
+  valueSecond: 'T',
+  contentFirst: '热情和敏感。',
+  contentSecond: '逻辑和明确'
+},{
+  title: '你把大部分和别人的相遇视为：',
+  valueFirst: 'F',
+  valueSecond: 'T',
+  contentFirst: '友善及重要的。',
+  contentSecond: '另有目的。'
+},{
+  title: '若你有时间和金钱,我的朋友邀请我到国外度假,并且在一天前才通知，我会：',
+  valueFirst: 'J',
+  valueSecond: 'P',
+  contentFirst: '必须先检查我的时间表',
+  contentSecond: '立即收拾行装'
+},{
+  title: '在第一次约会中:',
+  valueFirst: 'J',
+  valueSecond: 'P',
+  contentFirst: '若我所约的人来迟了, 我心里会很不高兴。',
+  contentSecond: '即使所约的人来迟了, 我点都不放在心里。'
+},{
+  title: '我偏好:',
+  valueFirst: 'J',
+  valueSecond: 'P',
+  contentFirst: '事先知道约会的行程:要去哪里、有谁参加我会在哪里多久、该如何打扮。',
+  contentSecond: '让约会自然地发生, 不做先前太多的计划。'
+},{
+  title: '我期待的生活包含着:',
+  valueFirst: 'J',
+  valueSecond: 'P',
+  contentFirst: '科学合理的安排和有毅力的执行。',
+  contentSecond: '没有什么压力,所有事情自然发生和时间弹性很大。'
+},{
+  title: '哪一项较为常见:',
+  valueFirst: 'J',
+  valueSecond: 'P',
+  contentFirst: '我先入会场而其它人随后来到。',
+  contentSecond: '大部分人都先到而我最后一个来。'
+},{
+  title: '我是这样喜欢....的人:',
+  valueFirst: 'J',
+  valueSecond: 'P',
+  contentFirst: '快刀斩乱麻,下定决心并且做出最后肯定的结论。',
+  contentSecond: '放开我的选择并且持续收集信息,这样结果会更准确和深刻。'
+},{
+  title: '我是这类型的人:',
+  valueFirst: 'J',
+  valueSecond: 'P',
+  contentFirst: '喜欢在一个时间里专心于一件事情直到完成。',
+  contentSecond: '喜欢同时进行好几件事'
+}]
+export default data;
